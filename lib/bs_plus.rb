959d9c92c35d09a9eecd198b9575fdd741ebbf1b
Leo <leonid.inbox@gmail.com>
2015-01-20 17:09:30 +0300

    some more 'require' juggling. god forgives

--- lib/bs_plus.rb
+++ lib/bs_plus.rb
@@ -1,10 +1,10 @@
-require 'bs_plus/version'
-require 'bs_plus/cli'
 require 'bs_plus/core_ext'
+require 'bs_plus/version'
 require 'bs_plus/config'
+BsPlus::Config.init # will ask for username/password if not present, hehe
 
+require 'bs_plus/cli'
 require 'bs_plus/browser'
 
 module BsPlus
-  Config.init # will ask for username/password if not present
 end
